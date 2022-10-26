35834b87b1117e2d5c5dd9250319d710d7854c0a
Lahcene Benmouloud <lahcenebenmouloud@gmail.com>
2022-10-26 14:05:44 +0100

    Ccm

---
+++ resources/views/ccm.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.front')
+@section('content')
+
+
+
+    <!-- Booking area starts -->
+    <section class="booking-area pat-100 pab-50">
+        <div class="container">
+            <div class="section-title center-text">
+                <h2 class="title"> l'investissement sans contraintes ! </h2>
+                <div class="section-title-shapes"> </div>
+            </div>
+            <div class="row gy-4 mt-5">
+                <div class="container py-5">
+                    <div class="main-timeline-4 text-white">
+                      <div class="timeline-4 left-4">
+                        <div class="card gradient-custom">
+                          <div class="card-body p-4">
+                            <i class=""></i>
+                            <i class="fas fa-user fa-2x mb-3"></i>
+                            <h4 style="color: #ffff">S'inscrire en quelques clics</h4>
+                            <p class="small text-white-50 mb-4 text-white" > Créer un compte Ethix </p>
+                            <p style="color: #ffff">Quelques informations et une copie de votre pièce d'identité seront nécessaires pour la création d'un compte investisseur.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="timeline-4 right-4">
+                        <div class="card gradient-custom-4">
+                          <div class="card-body p-4">
+                            <i class="fas fa-check fa-2x mb-3"></i>
+                            <h4 style="color: #ffff">Validation de votre compte</h4>
+                            <p class="small text-white-50 mb-4 text-white" >  Rejoindre le groupe d'investisseurs  </p>
+                            <p style="color: #ffff">Une fois l'inscription validée et le contrat Ethix dûment signé, vous rejoignez le groupe d’investisseurs Ethix.
+                                Vous avez désormais accès à votre tableau de bord et vous pouvez accéder aux informations concernant les investissements dans les projets en cours.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="timeline-4 left-4">
+                        <div class="card gradient-custom">
+                          <div class="card-body p-4">
+                            <i class="fas fa-euro-sign fa-2x mb-3"></i>
+                            <h4 style="color: #ffff"> Encaisser ou réinvestir sa plus-value </h4>
+                            <p class="small text-white-50 mb-4 text-white" >Récoltez les Fruits de vos investissements  </p>
+                            <p style="color: #ffff">Vous avez pris des parts dans un projet ? Récupérez vos gains lors de la vente de ce dernier et choisissez ou non de les réinvestir dans de nouveaux projets afin de bénéficier de l'effet cumulé.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Booking area ends -->
+    <!-- Booking area starts -->
+    <section class="booking-area pat-100 pab-50">
+        <div class="container">
+            <div class="section-title center-text">
+                <h2 class="title" > Nous vous proposons des projets clé en main </h2>
+                <div class="section-title-shapes"> </div>
+            </div>
+       
+            
+           
+        </div>
+    </section>
+    
+    <!-- Booking area ends -->
+    
+    
+    
+
+
+
+@endsection
--- resources/views/investor/investissements.blade.php
+++ resources/views/investor/investissements.blade.php
@@ -9,48 +9,39 @@
     <div class="dashboard-area section-bg-2 dashboard-padding">
         <div class="container">
             <div class="shop-contents-wrapper mt-5">
-                <div class="shop-icon">
-                    <div class="shop-icon-sidebar">
-                        <i class="las la-bars"></i>
-                    </div>
-                </div>
+               
                 <div class="shop-grid-contents">
+
+                    <div class="grid-list-contents">
+                        <div class="grid-list-contents-flex">
+                            <p class="grid-list-contents-para"> Trouvez ici tous vos invesstissements :   </p>
+                            
+                        </div>
+                    </div>
+
                     <div id="tab-grid" class="tab-content-item active mt-4">
                         <div class="row gy-4">
-                            @foreach($investissements as $investissement)
-                            <div class="col-md-6">
-                                <div class="hotel-view bg-white radius-20">
-                                    <a href="#" class="hotel-view-thumb hotel-view-grid-thumb bg-image" ><img src="{{asset('storage/images/properties/'.$investissement->property->images[0]->link)}}" alt="properties">
-                                    </a>
-                                    <div class="hotel-view-contents">
-                                        <div class="hotel-view-contents-header">
-                                          
-                                            <h3 class="hotel-view-contents-title"> <a href="#"> {{$investissement->property->designation}} </a> </h3>
-                                            <div class="hotel-view-contents-location mt-2">
-                                                <span class="hotel-view-contents-location-icon"> <i class="las la-map-marker-alt"></i> </span>
-                                                <span class="hotel-view-contents-location-para"> {{$investissement->property->addresse}}  </span>
-                                            </div>
-                                        </div>
-                                        
-                                        <div class="hotel-view-contents-bottom pt-2">
-                                            <div class="hotel-view-contents-bottom-flex">
-                                                <div class="hotel-view-contents-bottom-contents row">
-                                                    <div class="col-4">
-                                                        <h4  >{{$investissement->nbr_ethix}} Ethixs </h4> Achtés
-                                                    </div>
-                                                  </div>
-                                            </div>
-                                            
+                            <table class="table table-striped">
+                                <tbody>
+                                    @foreach($investissements as $investissement)
+                                    <tr  style="background-color:#ffff; margin-bottom:10px;">
+                                        <th scope="row" style="width: 100px;"> <img src="{{asset('storage/images/properties/'.$investissement->property->images[0]->link)}}" height="50px;" width="50px;" alt="properties"></th>
+                                        <td>  
+                                            <h5> <a href="#"> {{$investissement->property->designation}} </a> </h5> 
+                                            <span style="color: #667085"> {{$investissement->property->addresse}}  </span>
+                                        </td>
+                                        <td>
+                                            <h6 class="mt-2"> <span class="badge " style="background-color: #4DAA7F">{{$investissement->nbr_ethix}} Ethixs investis </span> </h6>
+                                        </td>
+                                        <td>
                                             <h6 class="mt-2 mb-1">{{$investissement->property->financing_percentage()}} % financé</h6>
-                                            <div class="progress mb-2">
-                                                <div class="progress-bar"  role="progressbar" style="width: {{$investissement->property->financing_percentage()}}%; background-color:#4daa7f" aria-valuenow="{{$investissement->property->financing_percentage()}}" aria-valuemin="0" aria-valuemax="100"></div>
-                                            </div>
+                                        </td>
+                                    </tr>
+                                  @endforeach
+                                </tbody>
+                            </table>
 
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
+                         
                         </div>
                        
                     </div>
