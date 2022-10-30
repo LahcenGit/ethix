a91ab45fba5a95013fdd12eb0e776618b2a8bfb9
Benosman Hind <62807209+BenosmanHind@users.noreply.github.com>
2022-10-30 12:42:55 +0100

    Update investissements.blade.php

--- resources/views/admin/investissements.blade.php
+++ resources/views/admin/investissements.blade.php
@@ -7,7 +7,7 @@
             <div class="col-sm-6 p-md-0">
                 <div class="welcome-text">
                     <h4 >Bonjour, Bienvenue!</h4>
-                    <span>Propriétés</span>
+                    <span>Investissements</span>
                 </div>
             </div>
             <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
