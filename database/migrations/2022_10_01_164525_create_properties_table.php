dbfa79c237c50afb0613f861e83e8fd39326c11f
Lahcene Benmouloud <lahcenebenmouloud@gmail.com>
2024-02-01 20:38:13 +0100

    Update 2022_10_01_164525_create_properties_table.php

--- database/migrations/2022_10_01_164525_create_properties_table.php
+++ database/migrations/2022_10_01_164525_create_properties_table.php
@@ -18,15 +18,29 @@ return new class extends Migration
             $table->string('designation');
             $table->string('addresse');
             $table->longText('description');
-            $table->string('valuation')->nullable();
+            $table->float('valuation')->nullable();
             $table->string('profitability');
-            $table->string('obj_revenu')->nullable();
-            $table->string('obj_financement');
+            $table->float('obj_revenu')->nullable();
+            $table->float('obj_financement');
             $table->string('type');
             $table->string('footage')->nullable();
             $table->integer('nbr_bedroom')->nullable();
             $table->integer('nbr_bathroom')->nullable();
+            $table->integer('nbr_ethix')->nullable();
+            $table->integer('reserve')->nullable();
+            $table->float('acquisition_price')->nullable();
+            $table->integer('remuneration_ethix')->nullable();
+            $table->float('notary_fees')->nullable();
+            $table->float('money_reserve_acquisition')->nullable();
+            $table->float('rent_collected')->nullable();
+            $table->float('charge')->nullable();
+            $table->integer('property_tax')->nullable();
+            $table->float('money_reserve_rendement')->nullable();
             $table->integer('max_ethix');
+
+            $table->float('total_valorisation')->nullable();
+            $table->float('total_acquisition')->nullable();
+
             $table->string('flag')->nullable();
             $table->string('slug')->nullable();
             $table->softDeletes();
